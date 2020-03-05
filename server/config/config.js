f5d20586833bfb90161764df6296e24b84e6051f
gvidal <gabrielwv1095@gmail.com>
2020-03-04 23:42:53 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 //=========
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==========
+//Entorno
+//=========
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==========
+//Base de datos
+//=========
+
+let urlDB;
+if (process.env.NODE_ENV === 'devi') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://gvidal:123@cluster0-et3nb.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
